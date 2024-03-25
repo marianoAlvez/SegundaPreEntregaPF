55d8ca8b59fda9a5ad7d7b0b560bb442d521f36b
Mariano Alvez <alvez_jmariano@hotmail.com>
2024-03-24 21:46:58 -0300

    Actualizar rutas y vistas de productos y carrito

--- src/routes/views.route.js
+++ src/routes/views.route.js
@@ -1,21 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productsModel = require('../models/mongo_models/products.schema.js');
+const productsModel = require("../models/mongo_models/products.schema.js");
+const cartsModel = require("../models/mongo_models/carts.schema.js");
 
 // GET /products
-router.get('/products', async (req, res) => {
-    let page = parseInt(req.query.page);
-    let limit = parseInt(req.query.limit) || 3; // default limit = 3
+router.get("/products", async (req, res) => {
+  let page = parseInt(req.query.page);
+  let limit = parseInt(req.query.limit) || 3; // default limit = 3
 
-      if (!page) page = 1 // default page = 1
-      let result = await productsModel.paginate({}, { page, limit: limit, lean: true })
-      
-      result.prevLink = result.hasPrevPage ? `http://localhost:8080/products?page=${result.prevPage}` : '';
-      result.nextLink = result.hasNextPage ? `http://localhost:8080/products?page=${result.nextPage}` : '';
-      
-      result.isValid = !(page < 1 || page > result.totalPages)
-      
-      res.render("products", result)
-  });
+  if (!page) page = 1; // default page = 1
+  let result = await productsModel.paginate(
+    {},
+    { page, limit: limit, lean: true }
+  );
 
-  module.exports = router;
+  result.prevLink = result.hasPrevPage
+    ? `http://localhost:8080/products?page=${result.prevPage}`
+    : "";
+  result.nextLink = result.hasNextPage
+    ? `http://localhost:8080/products?page=${result.nextPage}`
+    : "";
+
+  result.isValid = !(page < 1 || page > result.totalPages);
+
+  res.render("products", result);
+});
+
+// GET /cart/:cid
+router.get("/cart/:cid", async (req, res) => {
+  let cid = req.params.cid;
+  try {
+    let cart = await cartsModel.findById(cid).populate("products.product");
+    console.log(cart);
+    res.render("cart", cart);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
