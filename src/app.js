8d4f59084af3c5ce8f1bd398cfc67bbca5751d82
Mariano Alvez <alvez_jmariano@hotmail.com>
2024-03-27 20:33:54 -0300

    Refactoriza la configuración de Express y Handlebars, y organiza las rutas

--- src/app.js
+++ src/app.js
@@ -1,73 +1,38 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-const hbs = require("express-handlebars");
-const Handlebars = require('handlebars');
+// /** ★━━━━━━━━━━━★ Importa Configuraciones ★━━━━━━━━━━━★ */
+
+const app = require("./config/express");
+const path = require('path');
+const handlebars = require("./config/handlebars");
+const connectMongoDB = require("./config/db");
+
+// /** ★━━━━━━━━━━━★ Configurando Express ★━━━━━━━━━━━★ */
+
+app.engine('handlebars', handlebars.engine);
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views/"));
+
+// /** ★━━━━━━━━━━━★ Importa las rutas ★━━━━━━━━━━━★ */
+
 const productFsRouter = require("./routes/file_routes/products.route");
 const cartFsRouter = require("./routes/file_routes/carts.route");
 const productRoutes = require("./routes/db_routes/products.route");
 const cartRoutes = require("./routes/db_routes/carts.route");
 const viewsRouter = require("./routes/views.route");
 
-/** ★━━━━━━━━━━━★ Declarando Express ★━━━━━━━━━━━★ */
-const app = express();
+// /** ★━━━━━━━━━━━★ Configurando las rutas ★━━━━━━━━━━━★ */
 
-/** ★━━━━━━━━━━━★ Configurando Handlebars ★━━━━━━━━━━━★ */
-const handlebars = hbs.create({
-  defaultLayout: "main",
-  partialsDir: path.join(__dirname, 'views/partials'),
-  runtimeOptions: {
-    allowProtoPropertiesByDefault: true,
-    allowProtoMethodsByDefault: true,
-  },
-});
-
-// Helpers Handlebars
-Handlebars.registerHelper("add", function (value1, value2) {
-  return value1 + value2;
-});
-
-Handlebars.registerHelper("subtract", function (value1, value2) {
-  return value1 - value2;
-});
-
-Handlebars.registerHelper("multiply", function (value1, value2) {
-  return value1 * value2;
-});
-
-app.engine('handlebars', handlebars.engine);
-app.set("view engine", "handlebars");
-app.set("views", path.join(__dirname, "views/"));
-
-/** ★━━━━━━━━━━━★  Middlewares ★━━━━━━━━━━━★ */
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-/** ★━━━━━━━━━━━★ routes ★━━━━━━━━━━━★ */
-// con FileSystem
 app.use("/fs/products", productFsRouter);
 app.use("/fs/carts", cartFsRouter);
-// con MongoDB
 app.use("/api/products", productRoutes);
 app.use("/api/carts", cartRoutes);
-// con Handlebars
 app.use("/", viewsRouter);
 
-const port = 8080; //todo pasar a .env
+// /** ★━━━━━━━━━━━★ Iniciando el servidor ★━━━━━━━━━━━★ */
+
+const port = 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-/** ★━━━━━━━━━━━★ Configurando Mongo ★━━━━━━━━━━━★ */
-const connectMongoDB = async () => {
-  const stringConnection =
-    "mongodb://127.0.0.1:27017/ecommerce?retryWrites=true&w=majority"; //todo pasar a .env
-  try {
-    await mongoose.connect(stringConnection);
-    console.log("Conectado con exito a MongoDB usando Moongose.");
-  } catch (error) {
-    console.error("No se pudo conectar a la BD usando Moongose: " + error);
-    process.exit();
-  }
-};
+// /** ★━━━━━━━━━━━★ Conecta Mongo DB ★━━━━━━━━━━━★ */
 connectMongoDB();
