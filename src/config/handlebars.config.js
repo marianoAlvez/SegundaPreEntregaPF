5518dc8d0863e9a41b7a86f18a8dd5473e53ab35
Mariano Alvez <alvez_jmariano@hotmail.com>
2024-03-28 12:58:30 -0300

    Agrega configuraciones de Handlebars y rutas de archivos y base de datos

---
+++ src/config/handlebars.config.js
@@ -0,0 +1,38 @@
+// /** ★━━━━━━━━━━━★ Importa Configuraciones ★━━━━━━━━━━━★ */
+
+const app = require("./config/express");
+const path = require('path');
+const handlebars = require("./config/handlebars");
+const connectMongoDB = require("./config/db");
+
+// /** ★━━━━━━━━━━━★ Configurando Express ★━━━━━━━━━━━★ */
+
+app.engine('handlebars', handlebars.engine);
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views/"));
+
+// /** ★━━━━━━━━━━━★ Importa las rutas ★━━━━━━━━━━━★ */
+
+const productFsRouter = require("../routes/file_routes/products.route");
+const cartFsRouter = require("../routes/file_routes/carts.route");
+const productRoutes = require("../routes/db_routes/products.route");
+const cartRoutes = require("../routes/db_routes/carts.route");
+const viewsRouter = require("../routes/views.route");
+
+// /** ★━━━━━━━━━━━★ Configurando las rutas ★━━━━━━━━━━━★ */
+
+app.use("/fs/products", productFsRouter);
+app.use("/fs/carts", cartFsRouter);
+app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoutes);
+app.use("/", viewsRouter);
+
+// /** ★━━━━━━━━━━━★ Iniciando el servidor ★━━━━━━━━━━━★ */
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// /** ★━━━━━━━━━━━★ Conecta Mongo DB ★━━━━━━━━━━━★ */
+connectMongoDB();
